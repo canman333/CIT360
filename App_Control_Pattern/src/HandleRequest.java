fd08de2def73a6efa254e1dda526f32eabfb19a0
Lynn Mecham <lmecham@QualityTireCompany.local>
2017-02-18 20:55:01 -0700

    updating fileset

--- App_Control_Pattern/src/HandleRequest.java
+++
@@ -1,6 +0,0 @@
-
-//HandleRequest is an interface that executes the corresponding class tied to it's key
-public interface HandleRequest {
-	void execute(int bankNumber);
-
-}
--- systemTest/src/systemTest/sysTest.java
+++
@@ -1,6 +0,0 @@
-package systemTest;
-import junit.*;
-
-public class sysTest {
-
-}
